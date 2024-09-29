9f7ab9d9f9bb0015369e21bd90938ca090c7fdbe
DC_Jay <Harrisj22412@hocking.edu>
2024-09-29 16:30:05 +0000

    Refactor App component to fetch and display Pokemon data dynamically

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,13 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import PokemonList from './PokemonList';
 import axios from 'axios';
 
 function App() {
   const[pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
 
-  axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+  useEffect(() => {
+    setLoading(true)
+    axios.get(currentPageUrl).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
     setPokemon(res.data.results.map(p => p.name))
-  });
+  })
+ }, [currentPageUrl])
+
+ if(loading) return "Loading..."
+
   return (
    <PokemonList pokemon={pokemon} />
   );
